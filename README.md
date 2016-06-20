c17af719412b446c628526a1f79ec020f8b4957f
Dan Halliday <dan@danhalliday.com>
2016-06-20 12:23:51 +0100

    Pause event

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Dash SDK for JavaScript
 
+[![Build Status](https://travis-ci.com/dashaudio/dash-sdk-javascript.svg?token=j6b8xzvQsPodsJQUxCGM&branch=master)](https://travis-ci.com/dashaudio/dash-sdk-javascript)
+
 Dash's JavaScript SDK.
--- source/models/event/track/pause.js
+++ source/models/event/track/pause.js
@@ -1,5 +1,27 @@
-import { Event } from '../event';
+import { TrackEvent } from './track';
+import { EventStatus } from '../status';
 
-export class TrackPauseEvent extends Event {
+/**
+ * Track Pause denotes that a track was pause on a client app by a user. Uses schema.org's
+ * [SuspendAction](https://schema.org/SuspendAction) *The act of momentarily pausing a device or
+ * application (e.g. pause music playback or pause a timer)*.
+ */
+export class TrackPauseEvent extends TrackEvent {
+  /**
+   * Build a dictionary for this event.
+   * @return {Object} The dictionary
+   */
+  build() {
+    return {
+      "@type": "SuspendAction",
+      "@context": "http://schema.org",
 
+      actionStatus: EventStatus.Completed,
+      agent: super.agent(),
+      instrument: super.instrument(),
+      location: super.location(),
+      object: super.track(),
+      startTime: super.now()
+    }
+  }
 }
